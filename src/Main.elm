8445ca165e914ede8816b3f61e412923e347da4c
Carsten König <Carsten@gettingsharper.de>
2016-12-21 12:42:52 +0100

    can move sliders around

--- src/Main.elm
+++ src/Main.elm
@@ -28,25 +28,93 @@ type alias Line =
     }
 
 
-modelToBoard : Model -> Board
-modelToBoard model =
-    List.concat
-        [ List.map (lineToBoard False) (List.reverse model.prevLines)
-        , [ lineToBoard True model.currentLine ]
-        , List.map (lineToBoard False) model.nextLines
-        ]
+moveUp : Model -> Model
+moveUp model =
+    case model.prevLines of
+        [] ->
+            model
+
+        p :: ps ->
+            let
+                ( p2, v ) =
+                    pop p
+            in
+                { prevLines = ps
+                , currentLine = p2
+                , nextLines = push v model.currentLine :: model.nextLines
+                }
 
 
-lineToBoard : Bool -> Line -> List Cell
-lineToBoard isCurrent line =
-    List.concat
-        [ List.map Slider (List.reverse line.prevCells)
-        , if isCurrent then
-            [ Empty ]
-          else
-            []
-        , List.map Slider line.nextCells
-        ]
+moveDown : Model -> Model
+moveDown model =
+    case model.nextLines of
+        [] ->
+            model
+
+        n :: ns ->
+            let
+                ( n2, v ) =
+                    pop n
+            in
+                { prevLines = push v model.currentLine :: model.prevLines
+                , currentLine = n2
+                , nextLines = ns
+                }
+
+
+moveLeft : Model -> Model
+moveLeft model =
+    { prevLines = List.map moveLineLeft model.prevLines
+    , currentLine = moveLineLeft model.currentLine
+    , nextLines = List.map moveLineLeft model.nextLines
+    }
+
+
+moveRight : Model -> Model
+moveRight model =
+    { prevLines = List.map moveLineRight model.prevLines
+    , currentLine = moveLineRight model.currentLine
+    , nextLines = List.map moveLineRight model.nextLines
+    }
+
+
+moveLineLeft : Line -> Line
+moveLineLeft line =
+    case line.prevCells of
+        [] ->
+            line
+
+        p :: ps ->
+            { prevCells = ps
+            , nextCells = (p :: line.nextCells)
+            }
+
+
+moveLineRight : Line -> Line
+moveLineRight line =
+    case line.nextCells of
+        [] ->
+            line
+
+        n :: ns ->
+            { prevCells = (n :: line.prevCells)
+            , nextCells = ns
+            }
+
+
+pop : Line -> ( Line, String )
+pop line =
+    case line.nextCells of
+        s :: ss ->
+            ( { line | nextCells = ss }, s )
+
+        [] ->
+            ( line, "fail" )
+
+
+push : String -> Line -> Line
+push s line =
+    { line | nextCells = s :: line.nextCells }
 
 
 solvedModel : Model
@@ -223,5 +291,14 @@ update msg model =
         NoOp ->
             model
 
-        _ ->
-            Debug.log (toString msg) model
+        SlideLeft ->
+            moveLeft model
+
+        SlideRight ->
+            moveRight model
+
+        SlideUp ->
+            moveUp model
+
+        SlideDown ->
+            moveDown model
